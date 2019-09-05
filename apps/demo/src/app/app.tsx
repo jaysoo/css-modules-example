d851f5b088fba1140bbef9b5273d4b021e9fb8e0
Jack Hsu <jack.hsu@gmail.com>
2019-09-05 12:53:37 -0400

    initial commit

---
+++ apps/demo/src/app/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './app.module.css';
+
+export const App = () => {
+  return (
+    <p className={styles.app}>
+      Hello!
+    </p>
+  );
+};
+
+export default App;
